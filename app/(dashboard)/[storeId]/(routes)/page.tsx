3c960542bc6486cd944b34df25a9b5be7550024b
angelDova <Angelfcb01@yahoo.com>
2023-06-30 10:13:30 -0700

    update

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,7 +1,17 @@
-const DashboardPage = () => {
-  return <div>This is a Dashboard!</div>;
+import prismadb from "@/lib/prismadb";
+
+interface DashboardPageProps {
+  params: { storeId: string };
+}
+
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+    },
+  });
+
+  return <div className="">Active Store: {store?.name}</div>;
 };
 
 export default DashboardPage;
-
-// 2:03:45
